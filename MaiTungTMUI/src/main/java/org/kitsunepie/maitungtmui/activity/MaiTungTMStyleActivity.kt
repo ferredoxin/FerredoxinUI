a3f9dc9bef2cbeebaddc241a720d190e71214723
Ketal <ketal@qq.com>
2021-06-23 21:07:13 +0800

    Change transition

--- MaiTungTMUI/src/main/java/org/kitsunepie/maitungtmui/activity/MaiTungTMStyleActivity.kt
+++ MaiTungTMUI/src/main/java/org/kitsunepie/maitungtmui/activity/MaiTungTMStyleActivity.kt
@@ -1,20 +1,50 @@
 package org.kitsunepie.maitungtmui.activity
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.transition.ChangeBounds
+import android.transition.Fade
 import android.transition.TransitionManager
+import android.transition.TransitionSet
+import android.transition.TransitionValues
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import org.kitsunepie.maitungtmui.R
 import org.kitsunepie.maitungtmui.base.TitleAble
 import org.kitsunepie.maitungtmui.databinding.ActivityMaitungTmStyleBinding
+import kotlin.properties.Delegates
 
 abstract class MaiTungTMStyleActivity<T> : AppCompatActivity() where T : Fragment, T : TitleAble {
 
+    private val transition = TransitionSet().apply {
+        ordering = TransitionSet.ORDERING_SEQUENTIAL
+        addTransition(Fade(Fade.OUT))
+        addTransition(object : ChangeBounds() {
+            var width by Delegates.notNull<Int>()
+            override fun captureStartValues(transitionValues: TransitionValues) {
+                super.captureStartValues(transitionValues)
+                if (transitionValues.view == binding.title) {
+                    val rect = transitionValues.values["android:changeBounds:bounds"] as Rect
+                    width = rect.right - rect.left
+                }
+            }
+
+            override fun captureEndValues(transitionValues: TransitionValues) {
+                super.captureEndValues(transitionValues)
+                if (transitionValues.view == binding.title) {
+                    val rect = transitionValues.values["android:changeBounds:bounds"] as Rect
+                    rect.right = rect.left + width
+                }
+            }
+        })
+        addTransition(Fade(Fade.IN))
+    }
+
     open var showNavigationIcon: Boolean = true
         set(value) {
-            TransitionManager.beginDelayedTransition(binding.constraintLayout3)
-            binding.imageView3.isVisible = value
+            TransitionManager.beginDelayedTransition(binding.toolbar, transition)
+            binding.imageView3.visibility = if (value) View.VISIBLE else View.GONE
             field = value
         }
     abstract val fragment: T
